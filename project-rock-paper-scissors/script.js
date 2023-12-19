322690214f9e9ccdd1e02b9a84745d087865c6f9
daltond1019 <daltond1019@gmail.com>
2023-12-19 09:31:21 -0700

    Create the starting files and the JS algorithm
    I wrote out the steps I think the JS is going to have to take to do rock paper
    scissors. I pasted them as comments in the script.js. The whole game should take
    place inside of the debug tools.

---
+++ project-rock-paper-scissors/script.js
@@ -0,0 +1,18 @@
+//Greet the user
+
+console.log('Hello. Would you like to play a game?')
+
+//Instruct the user on protocol. You can enter rock, paper, or scissors as plain text.
+//Make it case insensetive.
+
+//After you prompt for input but before recieving it generate but don't print rock,
+//paper, or scissors. Probably with a random number generator and some loops. If the
+//number is 0-0.3 its rock if its 0.34-0.66 its paper, if its 0.67-1 its scissors. 
+//Store the result.
+
+//Take the input from the user (as a function?) and compare it to the returned 
+//random result. Proabably need to do nested if statements for each input from the 
+//user and each result or vice versa. There should be a more elegant way to do it. 
+//I can't think of one now.
+
+//print the results.
